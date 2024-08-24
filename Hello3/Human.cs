b6635ae2133b888b3c9226de1b67d7c52e563168
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:32:47 +0200

    Add project files.

---
+++ Hello3/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello3
+{
+    internal class Human
+    {
+        public string firstname;
+
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi , I´m {0}",firstname);
+        }
+    }
+}
---
+++ Hello3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+
+namespace Hello3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Pelle = new Human();
+            Pelle.firstname = "svenson";
+            Pelle.IntroduceMyself();
+
+        }
+
+
+
+            
+            
+            
+            
+            
+ 
+        }
+    }
+}
